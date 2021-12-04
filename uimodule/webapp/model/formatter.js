7162b2d762cabc95b08e67ecf20eb8c196de7b30
Franz Reitmayer <franz.reitmayer@reitmayer.eu>
2021-12-04 21:11:12 +0100

    fix: applicatio title

--- uimodule/webapp/model/formatter.js
+++ uimodule/webapp/model/formatter.js
@@ -2,7 +2,6 @@ sap.ui.define([], function() {
     "use strict";
     return {
         toUnixDate: function(sDate) {
-            console.log(typeof(sDate));
             return (sDate.getTime()).toFixed(0);
         }
     };
